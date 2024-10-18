2cb53dc314271d9cef9476d288bc4d2fc98aaa61
Andy Pillaca <apillacag@gmail.com>
2024-10-18 17:39:19 -0500

    new files

---
+++ IntermedioPython/Modulo3/1.2String_representaciones.py
@@ -0,0 +1,7 @@
+texto_1 = "Esto es un string"
+texto_2 = 'Esto es un string'
+texto_3 = """Esto tambien es un string
+Linea 2 considerado parte de un string
+Linea 3 considerado parte de un string"""
+
+print(texto_1)
---
+++ IntermedioPython/Modulo3/1.3String_operadores.py
@@ -0,0 +1,10 @@
+#concatenar +
+nombre = "Jorge"
+apellido = "Villavicencio"
+resultado = nombre + " " + apellido
+print(resultado)
+
+#Multiplicacion *
+palabra = "hola"
+print(palabra * 20) #Repetir la palabra 20 veces
+
